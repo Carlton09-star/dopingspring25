b40781bf39e5421a05dab7e088e3e2166f018599
Carlton09-star <tjjoyce09@gmail.com>
2025-08-25 11:18:28 -0400

    Delete numtol.m

--- matlab/matlab/numtol.m
+++
@@ -1,9 +0,0 @@
-function colLetter = numtol(num)
-    colLetter = '';
-    while num > 0
-        remainder = mod(num - 1, 26);
-        colLetter = [char(remainder + 'A'), colLetter];
-        num = floor((num - remainder - 1) / 26);
-    end
-end
-
