56205b420083a8ab3d28dc85ef1879cfe2661b0e
Carlton09-star <tjjoyce09@gmail.com>
2025-08-25 11:16:01 -0400

    Delete tables.m

--- matlab/matlab/tables.m
+++
@@ -1,9 +0,0 @@
-function []=tables(temp,time,source,sourcet,data)
-
-name1=sprintf('%d-%d-%d-%danalysis.csv',temp,time,source,sourcet);
-name2="D:\dopingspring25\data\analysis\";
-name=name2+name1;
-% Open a text file to write the table
-
-writematrix(data,name)
-end
