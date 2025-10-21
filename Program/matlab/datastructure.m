e0a28b54030f769449c9195fc0bb51a62047553f
Carlton09-star <tjjoyce09@gmail.com>
2025-10-21 18:54:33 -0400

    Finished the data structure work back to app design

--- Program/matlab/datastructure.m
+++ Program/matlab/datastructure.m
@@ -1,80 +1,94 @@
+% ---------- Helper Function for Adding Trials ----------
+
+function data = addDopingData(data, sourcet, substrate, hasPd, hasDn, Ptemp, Ptime, Dtemp, Dtime, IV_data, Rs_data, Cd_data)
+   function branch = addTrialBranch(branch, tempField, timeField, IV_data, Rs_data, Cd_data)
+    % Ensure subfields exist
+    if ~isfield(branch, tempField)
+        branch.(tempField)=struct();
+    end
+    if ~isfield(branch.(tempField), timeField)
+        branch.(tempField).(timeField)=struct();
+    end
+
+    % Find next available trial number
+    trials = fieldnames(branch.(tempField).(timeField));
+    if isempty(trials)
+        trialNum=1;
+    else
+        nums=cellfun(@(x)sscanf(x,'t%d'),trials);
+        trialNum=max(nums)+1;
+    end
+    trialField=sprintf('t%d',trialNum);
+
+    % Add the data
+    branch.(tempField).(timeField).(trialField)=struct('IV',IV_data,'Rs', Rs_data,'Cd', Cd_data);
+end
+
+
+% Helper function to add or update trial data in a doping experiment structure
+        %load(DopingData.mat   Will be added in once data is put in
+    % --- Ensure top-level fields exist ---
+    if ~isfield(data,sourcet)
+        data.(sourcet)=struct();
+    end
+    if ~isfield(data.(sourcet),substrate)
+        data.(sourcet).(substrate)=struct();
+    end
+
+    % --- Add Predeposition data if available ---
+    if hasPd
+        PdField="Ptemp"+num2str(Ptemp);
+        PtField="Ptime"+num2str(Ptime);
+
+        if ~isfield(data.(sourcet).(substrate), 'Pd')
+            data.(sourcet).(substrate).Pd=struct();
+        end
+        data.(sourcet).(substrate).Pd=addTrialBranch(data.(sourcet).(substrate).Pd, PdField, PtField, IV_data, Rs_data, Cd_data);
+    end
+
+    % --- Add Drive-in data if available ---
+    if hasDn
+        DdField="Dtemp"+num2str(Dtemp);
+        DtField="Dtime"+num2str(Dtime);
+
+        if ~isfield(data.(sourcet).(substrate),'Dn')
+            data.(sourcet).(substrate).Dn=struct();
+        end
+        data.(sourcet).(substrate).Dn=addTrialBranch(data.(sourcet).(substrate).Dn,DdField,DtField,IV_data,Rs_data,Cd_data);
+    end
+    %save("DopingData.mat",data)
+end
+
+
+
+
 clc 
 clear variables
+
 %Nested Structure for data storage
 Pretemp=900;
 Pretime=90;
+dtemp=1000;
+dtime=120;
 
 sourcet="TP470";
 substrate="Silicon";
 Ptemp="Ptemp"+num2str(Pretemp);
 Ptime="Ptime"+num2str(Pretime);
+dtemp="Dtemp"+num2str(dtemp);
+dtime="Dtime"+num2str(dtime);
 trialn="t"+num2str(1);
+Cd_data=1*10^(26);
+Rs_data=100;
+IV_data=[1,10,100,-1,-10,-100;2,20,200,-2,-20,-200];
 
 
 
 
 
-
-data=struct(sourcet,struct(substrate,struct('Pd',struct(Ptemp,struct(Ptime,struct(trialn,struct('Cd',[],'Rs',[],'IV',[])))),'Dn',struct(Ptemp,struct(Ptime,struct(trialn,struct('Cd',[],'Rs',[],'IV',[])))))));
-
-
-%% Haven't checked this next part yet need to test
+data=struct(sourcet,struct(substrate,struct('Pd',struct(Ptemp,struct(Ptime,struct(trialn,struct('Cd',Cd_data,'Rs',Rs_data,'IV',IV_data)))),'Dn',struct(Ptemp,struct(Ptime,struct(dtemp,struct(dtime,struct(trialn,struct('Cd',Cd_data,'Rs',Rs_data,'IV',IV_data)))))))));
 
 
-clc
-clear variables
 
-% --- Example inputs ---
-sourcet  = "TP470";   % could also be TP480, etc.
-substrate = "Silicon";
-Pretemp  = 900;
-Pretime  = 90;
-
-Ptemp = "Ptemp" + num2str(Pretemp);
-Ptime = "Ptime" + num2str(Pretime);
-
-% --- Initialize or load structure ---
-if exist('data','var')
-    % do nothing
-else
-    data = struct();
-end
-
-% --- Create source and substrate levels if they don't exist ---
-if ~isfield(data, sourcet)
-    data.(sourcet) = struct();
-end
-
-if ~isfield(data.(sourcet), substrate)
-    data.(sourcet).(substrate) = struct('Pd', struct(), 'Dn', struct());
-end
-
-% --- Check Pd level (we’ll focus on Pd for example) ---
-Pd = data.(sourcet).(substrate).Pd;
-
-if ~isfield(Pd, Ptemp)
-    Pd.(Ptemp) = struct();
-end
-if ~isfield(Pd.(Ptemp), Ptime)
-    Pd.(Ptemp).(Ptime) = struct();
-end
-
-% --- Determine next trial automatically ---
-trialNames = fieldnames(Pd.(Ptemp).(Ptime));
-if isempty(trialNames)
-    trialNum = 1;
-else
-    nums = cellfun(@(x) sscanf(x,'t%d'), trialNames);
-    trialNum = max(nums) + 1;
-end
-
-trialfield = sprintf('t%d', trialNum);
-
-% --- Add new trial ---
-Pd.(Ptemp).(Ptime).(trialfield) = struct('Cd', [], 'Rs', [], 'IV', []);
-
-% --- Write back to main structure ---
-data.(sourcet).(substrate).Pd = Pd;
-
-% --- Display result ---
-disp(['Added trial ' trialfield ' to ' sourcet ', ' substrate])
+data=addDopingData(data,"TP250","Silicon",true,true,1000,60,1200,120,[1,2,3,-1,-2,-3;10,20,30,-10,-20,-30],100,1*10^(-20));
+data=addDopingData(data,"TP250","Silicon",true,false,900,90,[],[],[1,2,3,10,20,30],1000,3*10^(100));
